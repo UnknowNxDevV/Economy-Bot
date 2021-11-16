483c9299dd2b49f7aaa3bad6d3fb7b4de7143e44
Glitch (antique-adaptable-repair) <none>
2021-11-16 05:33:11 +0000

    🐴🏣 Checkpoint ./commands/addmoney.js:38716558/32 ./commands/Removemoney.js:38716558/963 ./index.js:38716558/196 ./commands/dig.js:38716558/1068

---
+++ commands/dig.js
@@ -0,0 +1,19 @@
+exports.execute = async (client, message, args) => {
+    let users = [
+        "silver",
+        "rocks",
+        "gold",
+        "iron"
+    ];
+    let amount = Math.floor(Math.random() * 200) + 100;
+    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: true, cooldown: 15000, customName: "dig" });
+    if (beg.onCooldown) return message.reply(`You have already dug! Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
+    if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** a sand storm started and you got sand in your eyes LMAO!`);
+    else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** somehow rolled into your hands and you sold it for **${beg.amount}** 💸. Now you have **${beg.after}** 💸.`);
+};
+
+exports.help = {
+    name: "dig",
+    aliases: [],
+    usage: "dig"
+}
