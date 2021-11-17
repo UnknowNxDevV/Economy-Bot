582a38da3c3b6bc01d170f625faea8f7160cfff5
Glitch (antique-adaptable-repair) <none>
2021-11-17 05:39:21 +0000

    👗🏓 Checkpoint ./botConfig.js:38716558/61

--- botConfig.js
+++ botConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token: "OTA5MzI1NTY4MzkzMjMyNDg0.YZCpbQ.POaPgER5xWECXa4gHkeXTNJJwI4",
-    prefix: "k!",
+    token: "",
+    prefix: "",
     admins: [
         "889380477226614806"
 ],
