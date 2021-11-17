b1a54cf601e7393ded17f112c21415d2f1cc52e1
Glitch (antique-adaptable-repair) <none>
2021-11-17 08:13:46 +0000

    🛰🍏 Checkpoint ./index.js:38716558/91

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.shop = {
   laptop: {
-    cost: 2000
+    cost: 5000
   },
   mobile: {
     cost: 1000
   },
   pc: {
-    cost: 3000
+    cost: 10000
   }
 };
 const fs = require("fs");
