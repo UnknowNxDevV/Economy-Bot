eae12ef018d0a5e51b77a4aee1fa41b10ee21efd
Glitch (antique-adaptable-repair) <none>
2021-11-20 03:09:09 +0000

    🍴🚴 Checkpoint ./commands/vote.js:38716558/650

--- commands/vote.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-const i = '<:infomation:779736273639440394>'
-module.exports.run = async (bot, message, args) => {
-    const usertag = message.member;
-    let begembed = new MessageEmbed()
-    .setColor("BLUE")
-    .setDescription(`${i} **${usertag.user.username}** : Vote For Kcoins Here : https://top.gg/bot/911376535129325619/vote`)
-    message.channel.send(begembed).catch();
-}
