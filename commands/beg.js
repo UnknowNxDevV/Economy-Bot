fb1fe36647e7e17c1dd13fa02d857249b6d40300
Glitch (antique-adaptable-repair) <none>
2021-12-14 08:32:05 +0000

    🏐🎋 Checkpoint ./commands/beg.js:38716558/4536 ./commands/daily.js:38716558/123 ./commands/dig.js:38716558/37

--- commands/beg.js
+++ commands/beg.js
@@ -6,10 +6,10 @@ exports.execute = async (client, message, args) => {
         "Zero"
     ];
     let amount = Math.floor(Math.random() * 200) + 100;
-    let beg = client.eco.beg(client.ecoAddUser, amount, { canLose: true });
-    if (beg.onCooldown) return message.reply(`Begon Thot! Come back after ${beg.time.seconds} seconds.`);
-    if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** Begon Thot! Try again later.`);
-    else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** donated you **${beg.amount}** 💸. Now you have **${beg.after}** 💸.`);
+    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: true, cooldown: 15000, customName: "beg" });
+    if (beg.onCooldown) return message.reply(`You have already begged! Come back after ${beg.time.seconds} seconds.`);
+    if (beg.lost) return message.channel.send(`**${users[Math.floor(Math.random() * users.length)]}:** You were caught! You couldn't get money kiddo.`);
+    else return message.reply(`**${users[Math.floor(Math.random() * users.length)]}** you begged and got **${beg.amount}** .`);
 };
 
 exports.help = {
--- commands/daily.js
+++ commands/daily.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 3000) + 1000;
     let addMoney = client.eco.daily(client.ecoAddUser, amount);
     if (addMoney.onCooldown) return message.reply(`You have already claimed your daily credit. Come back after ${addMoney.time.hours} hours, ${addMoney.time.minutes} minutes & ${addMoney.time.seconds} seconds to claim it again.`);
-    else return message.reply(`You have claimed **${addMoney.amount}** 💸 as your daily credit & now you have **${addMoney.after}** 💸.`);
+    else return message.reply(`You have claimed **${addMoney.amount}** as your daily credit**.`);
 };
 
 module.exports.help = {
